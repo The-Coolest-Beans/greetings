341b874c0e5c43fb1059f03f8100ee335baa176f
tcaldwel@nmsu.edu <tcaldwel@nmsu.edu>
2016-03-24 14:07:09 -0600

    Update themes API to serve card template data from database.

--- routes/themes.js
+++ routes/themes.js
@@ -1,16 +1,23 @@
 var express = require('express');//using express module
 var router = express.Router();//
+var templates = require('../database/models/cardTemplates.js'); // get cardTemplates table from database
 
 //Include this module if you want the route secured -- i.e. user must be logged in.
 //var authCheck = require('./authCheck');
 //router.use(authCheck);
 
 router.get('/', function(req, res, next) {
-  res.send([
+  
+  templates.findAll({
+  		attributes: { exclude: ['createdAt', 'deletedAt', 'updatedAt'] }
+	}).then(function (cardTemplates) {
+		res.send(cardTemplates);
+	});
+  /*res.send([
     {name:'Happy Birthday', defaultText:'Happy Birthday!', mainColor: 'yellow', accentColor: 'teal'},
     {name:'Leap Day', defaultText:'Happy Leap Day!!!!', mainColor: 'blue', accentColor: 'yellow'},
     {name:'St. Patrick\'s Day', defaultText:'Happy St. Patrick\'s Day!', mainColor: 'green', accentColor: 'gold' },
-  ]);
+  ]);*/
 });
 
 module.exports = router;

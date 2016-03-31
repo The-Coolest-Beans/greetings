974dac3fe402c0a87ee8efa3638939a216dea4a9
Mikaela Steele <mikaela@steeleconsult.com>
2016-03-31 13:04:24 -0600

    Found way to grab user data in api. Hooked up login ability to the database (no longer hard coded). Non-verified users or banned users will be kept from logging into the system. Updated my documentation list.

--- database/models/users.js
+++ database/models/users.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var db = require('../db.js');
 
-/* 
+/*
 Tells sequelize how our database is set up so it
 knows which columns to convert into which JSON fields.
 */
@@ -9,7 +9,7 @@ knows which columns to convert into which JSON fields.
 // createdAt and updatedAt fields are automatically defined when using sequelize
 var Users = db.define('users', {
   id: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     field: 'id', // Will result in an attribute that is id when user facing but userId in the database
   	primaryKey: true
   },
@@ -25,13 +25,17 @@ var Users = db.define('users', {
     type: Sequelize.STRING,
     field: 'email'
   },
-  admin: {
+  adminTF: {
     type: Sequelize.BOOLEAN,
-    field: 'admin'
+    field: 'adminTF'
   },
-  banned: {
+  bannedTF: {
     type: Sequelize.BOOLEAN,
-    field: 'banned'
+    field: 'bannedTF'
+  },
+  verifiedTF: {
+    type: Sequelize.BOOLEAN,
+    field: 'verifiedTF'
   }
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var bcrypt = require('bcrypt-nodejs');
 var router = express.Router();
 var tokenSecret = require('./tokenSecret');
+var users = require('../database/models/users.js'); // get users table from database
 /* GET home page. */
 router
   .post('/', function(req, res) {
@@ -11,56 +12,95 @@ router
 
     //This is normally used when saving a password to the database.
     var hash = bcrypt.hashSync('greetings12');
+    console.log('hashed password: ', hash);
 
-    //Get user object from database with req.body.username (or whatever)
-    if(req.body.name == 'mikaela@steeleconsult.com')
-    {
-      user = {
-        name: 'mikaela@steeleconsult.com',
-        password: hash,
-        adminTF: true
-      };
-    }
-    else {
-      user = undefined;
-    }
+    //Get user object from database that matches the passed email
+    users.find({
+      where: {
+        email: req.body.name
+      }
+    }).then(function(foundUser) {
+      var user = foundUser.dataValues;
+      console.log('Authenticate this user: ', user);
 
-    if (!user || !user.name) {
-      res.json({
-        success: false,
-        message: 'Authentication failed. User not found.'
-      });
-    } else if (user) {
-
-      // check if password matches - this is where we hash the password
-      //if (user.password != req.body.password) {
-      if (!bcrypt.compareSync(req.body.password, user.password)) {
+      if (!user || !user.name) {
+        console.log('User wan\'t found.');
         res.json({
           success: false,
-          message: 'Authentication failed. Wrong password.'
+          message: 'Authentication failed. User not found.'
         });
-      } else {
+      } else if (user) {
+        // check if password matches - this is where we hash the password
+        //if (!bcrypt.compareSync(req.body.password, user.password)) { //TODO: replace the password compare with this when hashes are working.
+        if (req.body.password != user.password) {
+          console.log('User password doesn\'t match.');
+          console.log('Passed Password: ', req.body.password);
+          console.log('Saved Password: ', user.password);
+          res.json({
+            success: false,
+            message: 'Authentication failed. Wrong password.'
+          });
+        } else {
+          //check to see if the user is verified or banned
+          if (!user.verifiedTF) {
+            console.log('User isn\'t verified');
+            res.json({
+              success: false,
+              message: 'Authentication failed. User not verified.'
+            });
+            return;
+          } else if (user.bannedTF) {
+            console.log('User is banned.');
+            res.json({
+              success: false,
+              message: 'Authentication failed. User is banned.'
+            });
+            return;
+          }
+          console.log('User is verified and isn\'t banned.');
 
-        //removes the password from the user object before returning it
-        delete user['password'];
+          //If the password was correct and they are verified and not banned,
+          //Then continue to generate the authentication token.
 
-        // if user is found and password is right
-        // create a token
-        var token = jwt.sign(user, tokenSecret, {
-          expiresInMinutes: 1440 // expires in 24 hours
-        });
-        console.log("token:", token);
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token,
-          user: user
-        });
-      };
-    };//end else-if(user)
-  })//end .post
+          //removes the password from the user object before returning it
+          delete user['password'];
+          //We don't need these dates in the token
+          delete user['createdAt'];
+          delete user['updatedAt'];
 
-  ;
+          //This is the user data that will create the token
+          console.log('user object: ', user);
+
+          // create a token with the user data
+          var token = jwt.sign(user, tokenSecret, {
+            expiresInMinutes: 1440 // expires in 24 hours
+          });
+          console.log("token:", token);
+          // return the information including token as JSON
+          res.json({
+            success: true,
+            message: 'Enjoy your token!',
+            token: token,
+            user: user
+          });
+        };
+      }; //end else-if(user)
+    });
+
+    // if(req.body.name == 'mikaela@steeleconsult.com')
+    // {
+    //   user = {
+    //     name: 'mikaela@steeleconsult.com',
+    //     password: hash,
+    //     adminTF: true
+    //   };
+    // }
+    // else {
+    //   user = undefined;
+    // }
+
+  }) //end .post
+
+;
 
 module.exports = router;

18e4fd74eb685ad179a7e39f3d42a2638dd900c7
tcaldwel@nmsu.edu <tcaldwel@nmsu.edu>
2016-04-30 19:35:54 -0600

    Update delete card API

--- routes/deleteCard.js
+++ routes/deleteCard.js
@@ -20,10 +20,20 @@ router.patch('/', function(req, res, next){
     deletedAt : new Date().getDate() + new Date().getTime(), //getting time stamp
   }, {
     where: {
-      id:req.body.id,//checking if the id requested
+      id: req.body.id,//checking if the id requested
       ownerId: user.id, //can't delete someone else's card!
     }//closing where-in
-  })//closing update
+  }).catch(function (err) {
+    res.status(500).send({
+        success: false,
+        message: err
+    });
+  }).then(function(){
+  	return res.status(200).send({//200 indicates 'Ok'
+      success: true,
+      message: 'Update successful'
+  	});
+  })
 });//closing delete
 
 module.exports = router;

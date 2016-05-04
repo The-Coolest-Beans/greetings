b9cb5e996bca980c9309afb8dec3933761a89a45
tcaldwel@nmsu.edu <tcaldwel@nmsu.edu>
2016-05-04 02:43:09 -0600

    Update admin page controller with new functionality

--- public/scripts/controllers/admin.js
+++ public/scripts/controllers/admin.js
@@ -29,7 +29,41 @@ angular
         authService.logOut();
       };
 
-      console.log($scope.user);
+      $http.get('/api/users').then(function(result){
+        console.log('user call result:', result);
+        if(!result.data || result.status != 200)
+        {
+          console.log('Failed to get users. Error: ', result);
+        }
+        else {
+          $scope.users = result.data;
+        }
+      }, function(e) {
+        console.log('Get call to users errored.', e);
+      }); // end api call block
+
+      $scope.sortType = "loggedIn"; // User field in table to order by
+      $scope.sortReverse = true;  // Reverse order table column
+
+      // Ban a user given their user ID
+      $scope.banUser = function(userId){
+        $http.patch('/api/users/banAccount/' + userId).then(function(result) {
+          console.log('User has been banned.', result);
+        }, function(e) {
+          // error occurred - print it
+          console.log('Error banning user.', e);
+        });
+      }
+
+      // Unban a user given their user ID
+      $scope.unbanUser = function(userId){
+        $http.patch('/api/users/unbanAccount/' + userId).then(function(result) {
+          console.log('User has been unbanned.', result);
+        }, function(e) {
+          // error occurred - print it
+          console.log('Error unbanning user.', e);
+        });
+      }
 
     } // end function
 

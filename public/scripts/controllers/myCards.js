4563af94374b28f905fd249c744dedd5474330da
Adam N. Smith <adamatrr@gmail.com>
2016-05-03 22:18:25 -0600

    Added statistics to my cards page. Also made the page look pretty when there are no cards.

--- public/scripts/controllers/myCards.js
+++ public/scripts/controllers/myCards.js
@@ -2,8 +2,8 @@
 
 angular
   .module('app')
-  .controller('myCardsCtrl', ['$stateParams', '$scope', '$rootScope', '$http', 'authService', '$timeout',
-    function($stateParams, $scope, $rootScope, $http, authService, $timeout) {
+  .controller('myCardsCtrl', ['$stateParams', '$scope', '$rootScope', '$http', 'authService', '$timeout', '$state',
+    function($stateParams, $scope, $rootScope, $http, authService, $timeout, $state) {
       console.log('Starting myCardsCtrl...');
 
       //This is set to handle new logins and already loaded users.
@@ -36,6 +36,8 @@ angular
         $scope.sentCards = result.data;
         $scope.firstSentCard = $scope.sentCards[0];
         $scope.sentCards.splice(0, 1);
+        $scope.currentlySelectedSentCard = $scope.firstSentCard;
+        $scope.currentlySelectedSentIndex = 0;
 
       }, function(e) {
 
@@ -43,6 +45,30 @@ angular
         console.log('Get call to sent cards errored.', e);
 
       }); // end api call block
+
+      $scope.lastSentClicked = function(){
+
+        // update the current counter index
+        if ($scope.currentlySelectedSentIndex <= 0) $scope.currentlySelectedSentIndex = $scope.sentCards.length;
+        else $scope.currentlySelectedSentIndex--;
+
+        // update the currently selected card
+        if ($scope.currentlySelectedSentIndex == 0) $scope.currentlySelectedSentCard = $scope.firstSentCard;
+        else $scope.currentlySelectedSentCard = $scope.sentCards[$scope.currentlySelectedSentIndex-1];
+
+      } // end method
+
+      $scope.nextSentClicked = function(){
+
+        // update the current counter index
+        if ($scope.currentlySelectedSentIndex >= $scope.sentCards.length) $scope.currentlySelectedSentIndex = 0;
+        else $scope.currentlySelectedSentIndex++;
+
+        // update the currently selected card
+        if ($scope.currentlySelectedSentIndex == 0) $scope.currentlySelectedSentCard = $scope.firstSentCard;
+        else $scope.currentlySelectedSentCard = $scope.sentCards[$scope.currentlySelectedSentIndex-1];
+
+      } // end method
 
       // clear out the templates array
       $scope.receivedCards = [];
@@ -54,6 +80,8 @@ angular
         $scope.receivedCards = result.data;
         $scope.firstReceivedCard = $scope.receivedCards[0];
         $scope.receivedCards.splice(0, 1);
+        $scope.currentlySelectedReceivedCard = $scope.firstReceivedCard;
+        $scope.currentlySelectedReceivedIndex = 0;
 
       }, function(e) {
 
@@ -61,6 +89,39 @@ angular
         console.log('Get call to sent cards errored.', e);
 
       }); // end api call block
+
+      $scope.lastReceivedClicked = function(){
+
+        // update the current counter index
+        if ($scope.currentlySelectedReceivedIndex <= 0) $scope.currentlySelectedReceivedIndex = $scope.receivedCards.length;
+        else $scope.currentlySelectedReceivedIndex--;
+
+        // update the currently selected card
+        if ($scope.currentlySelectedReceivedIndex == 0) $scope.currentlySelectedReceivedCard = $scope.firstReceivedCard;
+        else $scope.currentlySelectedReceivedCard = $scope.receivedCards[$scope.currentlySelectedReceivedIndex-1];
+
+      } // end method
+
+      $scope.nextReceivedClicked = function(){
+
+        // update the current counter index
+        if ($scope.currentlySelectedReceivedIndex >= $scope.receivedCards.length) $scope.currentlySelectedReceivedIndex = 0;
+        else $scope.currentlySelectedReceivedIndex++;
+
+        // update the currently selected card
+        if ($scope.currentlySelectedReceivedIndex == 0) $scope.currentlySelectedReceivedCard = $scope.firstReceivedCard;
+        else $scope.currentlySelectedReceivedCard = $scope.receivedCards[$scope.currentlySelectedReceivedIndex-1];
+
+      } // end method
+
+      $scope.extractEmailAddress = function(fromAddress){
+
+        if(fromAddress == null) return "";
+
+        var emailAddress = fromAddress.substring(fromAddress.lastIndexOf("<")+1, fromAddress.lastIndexOf(">"));
+        return emailAddress;
+
+      } // end method
 
       // method called when a card is delted from the mycards page
       $scope.deleteCard = function(index) {
@@ -75,12 +136,8 @@ angular
         // actually call the API to delete the card
         $http.patch('/api/deleteCard', 'id=' + cardToDelete.cardId, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } } ).then( function(result){
 
-          // remove the card from the carousel
-          var carousel = $('#sentCardCarousel');
-          var ActiveElement = carousel.find('.item.active');
-          ActiveElement.remove();
-          var NextElement = carousel.find('.item').first();
-          NextElement.addClass('active');
+          // reload the page
+          $state.reload();
 
         }, function(e) {
 

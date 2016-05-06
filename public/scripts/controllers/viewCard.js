3400b14c0d0daaa45714f0c4b96340b08d929faa
Mikaela Steele <mikaela@steeleconsult.com>
2016-05-06 15:38:34 -0600

    Added logic on viewCard page to get a deleted card if the user is an admin (not tested yet).

--- public/scripts/controllers/viewCard.js
+++ public/scripts/controllers/viewCard.js
@@ -5,7 +5,20 @@ angular
   .controller('viewCardCtrl', ['$stateParams', '$scope', '$rootScope', '$http', 'authService', '$timeout', '$state',
     function AppCtrl($stateParams, $scope, $rootScope, $http, authService, $timeout, $state) {
 
-
+      //This is set to handle new logins and already loaded users.
+      $scope.user = authService.getUser();
+      $scope.$on('UserAuthenticated', function() {
+        $timeout(function() {
+          $scope.user = authService.getUser();
+          console.log('Heard UserAuthenticated in layoutCtrl', $scope.user);
+        });
+      });
+      $scope.$on('UserLogOut', function() {
+        $timeout(function() {
+          $scope.user = authService.getUser();
+          console.log('Heard UserLogOut in layoutCtrl', $scope.user);
+        });
+      });
       $scope.logout = function() {
         console.log('Logging out.');
         authService.logOut();
@@ -36,6 +49,42 @@ angular
 
         }
       }, function(e) {
+
+        //If the user is an admin, try to load it with the admin call (ignores deletedDT).
+        if($scope.user && $scope.user.adminTF) {
+          console.log('User is admin, try again.');
+          $http.get('/api/admin/getSingleSent/' + $scope.cardID).then(function(result){
+            console.log('getSingleSent call result:', result);
+            if(!result.data || result.status != 200)
+            {
+              console.log('Failed to get card. Error: ', result);
+            }
+            else {
+              $scope.card = result.data;
+              console.log('Card data: ', $scope.card);
+              $scope.customStyle = {"color":$scope.card.headerTextColor, "font-family":$scope.card.fontFamily};
+
+              // get the template information
+              $http.get('/api/templates/' + $scope.card.templateId).then(function(result){
+                $scope.template = result.data[0];
+                console.log('Template data: ', $scope.template);
+              }, function(e) {
+                console.log('Get call to templates errored.', e);
+              }); // end api call block
+
+            }
+          }, function(e) {
+            console.log('Get call to getSingleSent errored.', e);
+            noty({
+              timeout: 3000,
+              type: 'error', //blue. Also alert, information, confirm, error, warning
+              layout: 'topCenter',
+              text: e.message,
+              closeWith: ['button', 'click'],
+            }); // end noty block
+          }); // end api call block
+        }
+
         console.log('Get call to cards errored.', e);
         noty({
           timeout: 3000,
